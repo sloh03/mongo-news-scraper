7204b54838a709ebf6e5eea33a17b33f57c537d5
Stefanie Loh <stefkloh@gmail.com>
2018-06-01 19:38:42 -0700

    Enabling hosting on heroku

--- server.js
+++ server.js
@@ -27,8 +27,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoNews";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/h");
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 // Routes
 
